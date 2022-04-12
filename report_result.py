922a17d8c0bcd94e9625b1a64d637452140d9205
Naehi Kim <57555570+eanihmik@users.noreply.github.com>
2022-04-12 16:59:36 +0900

    Create report_result.py

---
+++ report_result.py
@@ -0,0 +1,24 @@
+import numpy as np
+def solution(id_list, report, k):
+    report = np.unique(report)
+    
+    reporter = []
+    reported = []
+    for r in report:
+        reporter.append(r.split()[0])
+        reported.append(r.split()[1])
+        
+    d = {}
+    for i in np.unique(reported):
+        d[i] = reported.count(i)
+        
+    blocked = np.array(list(d.keys()))[np.array(list(d.values())) >= k]
+    
+    answer = []
+    for id in id_list:
+        count = 0
+        for n in range(len(reporter)):
+            if reported[n] in blocked and reporter[n] == id:
+                count += 1
+        answer.append(count)
+    return answer
